bf037752ee8d39f7e11487498c1b4e1d05391c8a
noway0032@gmail.com <noway0032@gmail.com>
2018-09-17 18:21:55 +0200

    bsIntegraition

--- elsoprojekt/src/app/app.component.ts
+++ elsoprojekt/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elsoprojekt';
+  tomb = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  constructor() {
+    console.log(this.tomb.filter(function (number) {
+      return number === 2;
+    }));
+    console.log(this.tomb.filter((number: number) => number === 2));
+    console.log(this.tomb.map((number: number) => number * 2));
+    console.log(this.tomb.reduce((x, y) => x + y));
+  }
 }
